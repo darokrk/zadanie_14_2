b5c86fbcf06b559acc9a6b652e9ee3138812bcbe
Dariusz Rosiek <daro.krk@interia.pl>
2018-02-27 14:12:15 +0100

    Move file script.js to folder /js, add an Id for movies elements

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ var serials = [
 ];
 
 var moviesElements = movies.map(function(movie) {
-	return React.createElement('li', {},
+	return React.createElement('li', {key: movie.id},
 		React.createElement('h2', {}, movie.title),
 		React.createElement('p', {}, movie.desc),
 		React.createElement('img', {src: movie.img})
